b5dd1ef89071046d07544a2627c3e038a229bde2
Joe Race <joe.race@vivoinspire.com>
2016-02-24 15:47:39 +0000

    Remove isOnline check - going to handle inside mobile and web apps using interceptors

--- src/http.js
+++ src/http.js
@@ -11,30 +11,25 @@ function HTTPService($rootScope, $http, $q) {
   var offlineError = {online: false};
 
   function callHTTP(config, success, fail) {
-    var deferred = $q.defer(),
-        isOnline = m_isBoolean(navigator.onLine) ? navigator.onLine : true;
+    var deferred = $q.defer();
 
     config = config || {};
     config.method = config.method || METHODS.read;
 
-    if (isOnline === false) {
-      fail(offlineError);
-      deferred.reject(offlineError);
-    } else {
-      $http(config)
-        .success(function (data) {
-          if (m_isFunction(success)) {
-            success(data);
-          }
-          deferred.resolve(data);
-        })
-        .error(function (data) {
-          if (m_isFunction(fail)) {
-            fail(data);
-          }
-          deferred.reject(data);
-        });
-    }
+    $http(config)
+      .success(function (data) {
+        if (m_isFunction(success)) {
+          success(data);
+        }
+        deferred.resolve(data);
+      })
+      .error(function (data) {
+        if (m_isFunction(fail)) {
+          fail(data);
+        }
+        deferred.reject(data);
+      });
+
     return deferred;
   }
 
@@ -70,7 +65,7 @@ function HTTPService($rootScope, $http, $q) {
 
   return {
     METHODS: METHODS,
-    
+
     call: callHTTP,
 
     read: callRead,
@@ -78,7 +73,7 @@ function HTTPService($rootScope, $http, $q) {
     change: callChange,
     create: callCreate,
     delete: callDelete,
-    
+
     readList: callRead,
     updateList: callUpdate,
     changeList: callChange,
